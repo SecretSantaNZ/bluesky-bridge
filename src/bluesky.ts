39e2fa8a00e9ae3d58edc522bba63a541717bf5f
Ed Costello <edc.open@gmail.com>
2024-05-25 16:06:41 +1200

    zod validation and dm poc

--- src/bluesky.ts
+++ src/bluesky.ts
@@ -1,6 +1,9 @@
 import { BskyAgent } from '@atproto/api';
 
-export const agent = new BskyAgent({ service: 'https://bsky.social' });
+export const agent = new BskyAgent({
+  service: 'https://bsky.social',
+  // service: 'https://public.api.bsky.app',
+});
 export const getBskyAgent = async () => {
   if (!agent.hasSession) {
     console.log('logging in');
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,108 @@
+import path from 'path';
 import dotenv from 'dotenv';
 import Fastify from 'fastify';
-import basicAuth from '@fastify/basic-auth';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import z from 'zod';
 
-dotenv.config();
+import { getBskyAgent } from './bluesky.js';
+
+dotenv.config({
+  path: [
+    path.resolve(process.cwd(), '.env'),
+    `${process.env.CREDENTIALS_DIRECTORY}/bluesky-bridge-creds`,
+  ],
+});
 
 const fastify = Fastify({
   logger: true,
 });
 
-fastify.register(basicAuth, {
-  validate: async (username, password) => {
-    if (username !== 'Tyrion' || password !== 'wine') {
-      return new Error('Winter is coming');
-    }
-  },
-  authenticate: { realm: 'Westeros' },
-});
-
-fastify.after(() => {
-  fastify.addHook('onRequest', fastify.basicAuth);
-});
+// Add schema validator and serializer
+fastify.setValidatorCompiler(validatorCompiler);
+fastify.setSerializerCompiler(serializerCompiler);
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
-  await new Promise((resolve) => setTimeout(() => resolve(undefined), 3000));
-  reply.send({ hello: 'world' });
+fastify.withTypeProvider<ZodTypeProvider>().get('/', async (request, reply) => {
+  reply.send({ ok: true });
 });
+fastify
+  .withTypeProvider<ZodTypeProvider>()
+  .get('/action/dm', async (request, reply) => {
+    const client = await getBskyAgent();
+    // const result = client.session;
+    // https://plc.directory/did:plc:crngjmsdh3zpuhmd5gtgwx6q
+    // const result = await client.api.app.bsky.graph.getFollowers({
+    //   actor: 'did:plc:crngjmsdh3zpuhmd5gtgwx6q',
+    // });
+    const result = await client.api.chat.bsky.convo.getConvoForMembers(
+      {
+        members: [
+          'did:plc:crngjmsdh3zpuhmd5gtgwx6q',
+          'did:plc:kj53ykzin4kn3oemv52myygh',
+        ],
+      },
+      {
+        headers: {
+          'atproto-proxy': 'did:web:api.bsky.chat#bsky_chat',
+        },
+      }
+    );
+    // await client.post({
+    //   text: 'this is a test, for reasons\n\nHello @orthanc.bsky.social\n\n',
+    //   facets: [
+    //     {
+    //       features: [
+    //         {
+    //           $type: 'app.bsky.richtext.facet#mention',
+    //           did: 'did:plc:k626emd4xi4h3wxpd44s4wpk',
+    //         },
+    //       ],
+    //       index: {
+    //         byteStart: 35,
+    //         byteEnd: 55,
+    //       },
+    //     },
+    //   ],
+    // });
+    reply.send(result);
+  });
+
+// Declare a route
+fastify.withTypeProvider<ZodTypeProvider>().post(
+  '/',
+  {
+    schema: {
+      body: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  async (request, reply) => {
+    const client = await getBskyAgent();
+    // await client.post({
+    //   text: request.body.message,
+    //   // facets: [
+    //   //   {
+    //   //     features: [
+    //   //       {
+    //   //         $type: 'app.bsky.richtext.facet#mention',
+    //   //         did: 'did:plc:k626emd4xi4h3wxpd44s4wpk',
+    //   //       },
+    //   //     ],
+    //   //     index: {
+    //   //       byteStart: 35,
+    //   //       byteEnd: 55,
+    //   //     },
+    //   //   },
+    //   // ],
+    // });
+    reply.send({ ok: true });
+  }
+);
 
 // Run the server!
 fastify.listen({ port: 3000 }, (err, address) => {
