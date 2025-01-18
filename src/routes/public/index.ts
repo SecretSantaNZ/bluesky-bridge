9ac1b71b6f8bef73964b89d97b43f88d7bca3132
Ed Costello <edc.open@gmail.com>
2025-01-18 17:13:33 +1300

    keep player logged in on rules and faq

--- src/routes/public/index.ts
+++ src/routes/public/index.ts
@@ -1,17 +1,41 @@
 import type { FastifyPluginAsync } from 'fastify';
 import * as dateUtils from '../../lib/dates.js';
+import { validateAuth } from '../../util/validateAuth.js';
+import { UnauthorizedError } from 'http-errors-enhanced';
 
 export const publicContent: FastifyPluginAsync = async (app) => {
+  app.addHook('onRequest', async function (request) {
+    try {
+      await validateAuth(
+        ({ authTokenManager }) => authTokenManager,
+        'session'
+      ).bind(this)(request);
+    } catch (e) {
+      // we ignore unauthorized errors as these pages are public so user is allowed
+      // to be not logged in
+      if (!(e instanceof UnauthorizedError)) {
+        throw e;
+      }
+    }
+  });
+
   app.addHook('preValidation', async function (request, reply) {
-    const settings = await this.blueskyBridge.db
-      .selectFrom('settings')
-      .selectAll()
-      .executeTakeFirstOrThrow();
+    const playerDid = request.tokenSubject;
+    const [player, settings] = await Promise.all([
+      playerDid == null
+        ? undefined
+        : app.blueskyBridge.playerService.getPlayer(playerDid),
+      this.blueskyBridge.db
+        .selectFrom('settings')
+        .selectAll()
+        .executeTakeFirstOrThrow(),
+    ]);
 
     reply.locals = {
       ...reply.locals,
       ...dateUtils,
-      player: undefined,
+      csrfToken: request.tokenData?.csrfToken,
+      player,
       settings,
     };
   });
