9133a4aeec6408708719dbb76e99360255a2bb24
Ed Costello <edc.open@gmail.com>
2025-02-07 22:14:33 +1300

    add mastodon players

--- src/routes/mastodon/index.ts
+++ src/routes/mastodon/index.ts
@@ -8,7 +8,10 @@ import type {
 import type { ZodTypeProvider } from 'fastify-type-provider-zod';
 import { z } from 'zod';
 import { safeFetchWrap } from '@atproto-labs/fetch-node';
-import wretch from 'wretch';
+import wretch, {
+  type WretchErrorCallback,
+  type WretchResponseChain,
+} from 'wretch';
 import FormDataAddon from 'wretch/addons/formData';
 import { BadRequestError } from 'http-errors-enhanced';
 import { unauthenticatedAgent } from '../../bluesky.js';
@@ -20,7 +23,7 @@ const w = wretch()
   })
   .addon(FormDataAddon)
   .options({
-    redirect: 'error',
+    redirect: 'manual',
   });
 
 const registrationResponseSchema = z.object({
@@ -41,6 +44,90 @@ const mastoUserSchema = z.object({
   acct: z.string(),
 });
 
+const webfingerSchema = z.object({
+  subject: z.string(),
+  links: z.array(
+    z.object({
+      rel: z.string(),
+      type: z.string().optional(),
+      href: z.string().optional(),
+    })
+  ),
+});
+
+export function getBridgedHandle(mastodon_account: string) {
+  return `${mastodon_account.replace(/[_~]/g, '-').replace('@', '.')}.ap.brid.gy`;
+}
+
+const followOneRedirect = <T, C>(
+  chain: C & WretchResponseChain<T, C, undefined>
+): C & WretchResponseChain<T, C, undefined> => {
+  let redirectCount = 0;
+  const callback: WretchErrorCallback<T, C, undefined> = (error, req) => {
+    const location = error.response.headers.get('location');
+    if (location == null) throw error;
+    if (redirectCount > 0) {
+      throw new Error(
+        `Only a single redirect is allowed but tried ${location} from ${error.url}`
+      );
+    }
+    redirectCount++;
+    const locationUrl = new URL(location);
+    const initialLocationUrl = new URL(error.url);
+    locationUrl.host = initialLocationUrl.host;
+    if (locationUrl.href !== initialLocationUrl.href) {
+      throw new Error(
+        `Redirect is only allowed to change host but tried ${location} from ${error.url}`
+      );
+    }
+    return req.url(location).get().json();
+  };
+  return chain
+    .error(301, callback)
+    .error(302, callback)
+    .error(307, callback)
+    .error(308, callback);
+};
+
+export async function resolveMastodonHandle(
+  handle: string,
+  host: string,
+  resolvesRemaining = 1
+) {
+  const webfingerUrl = new URL('/.well-known/webfinger', `https://${host}`);
+  const resource = `acct:${handle}`;
+  webfingerUrl.searchParams.set('resource', resource);
+
+  const webfingerResult = webfingerSchema.parse(
+    await followOneRedirect(w.get(webfingerUrl.href)).json()
+  );
+  if (webfingerResult.subject.toLowerCase() !== resource.toLowerCase()) {
+    throw new Error(
+      `Invalid subject, expected ${resource} but was ${webfingerResult.subject}`
+    );
+  }
+
+  const selfLinkRecord = webfingerResult.links.find(
+    (link) =>
+      link.rel === 'self' &&
+      (link.type ===
+        'application/ld+json; profile="https://www.w3.org/ns/activitystreams"' ||
+        link.type === 'application/activity+json')
+  );
+  if (selfLinkRecord == null || selfLinkRecord.href == null) {
+    throw new Error(`No self link for ${resource}`);
+  }
+  const selfUrl = new URL(selfLinkRecord.href);
+  if (selfUrl.host === host) {
+    const resolvedHandle = webfingerResult.subject.substring('acct:'.length);
+    return { host, handle: resolvedHandle };
+  }
+  if (resolvesRemaining <= 0) {
+    throw new Error(`Too many resolves for ${resource}`);
+  }
+  return resolveMastodonHandle(handle, selfUrl.host);
+}
+
 export async function startMastodonOauth(
   request: FastifyRequest,
   reply: FastifyReply,
@@ -52,28 +139,26 @@ export async function startMastodonOauth(
   const baseUrl = process.env.PUBLIC_BASE_URL as string;
   const redirectUri = `${baseUrl}/mastodon/callback`;
   const { db, playerService } = blueskyBridge;
-  const handle = rawHandle
-    .replace(/^@/, '')
-    .replace(/^https:\/\//, '')
-    .replace(/\/$/, '')
-    .trim();
-  const instance = handle.split('@').pop();
-  if (instance == null) {
+  const handle = rawHandle.replace(/^@/, '').trim();
+  const [username, instance] = handle.split('@');
+  if (!username || !instance) {
     throw new BadRequestError('Cannot parse instance');
   }
+
+  const { host } = await resolveMastodonHandle(handle, instance);
 
   const {
     subject: requestId,
     data: { returnUrl },
   } = await blueskyBridge.returnTokenManager.validateToken(returnToken);
 
-  let clientKey = instance;
+  let clientKey = host;
   let client_name = baseUrl;
   let scopes = 'profile';
   if (
     handle.toLowerCase() === playerService.santaMastodonHandle.toLowerCase()
   ) {
-    clientKey = instance + ':full-access';
+    clientKey = clientKey + ':full-access';
     client_name = baseUrl + ' - full-access';
     scopes = 'read write profile';
   }
@@ -84,7 +169,7 @@ export async function startMastodonOauth(
     .where('instance', '=', clientKey)
     .executeTakeFirst();
   if (client == null) {
-    const registerUrl = new URL('/api/v1/apps', `https://${instance}`).href;
+    const registerUrl = new URL('/api/v1/apps', `https://${host}`).href;
     const registration = registrationResponseSchema.parse(
       await w
         .post(
@@ -100,7 +185,7 @@ export async function startMastodonOauth(
     );
 
     client = {
-      instance,
+      instance: clientKey,
       ...registration,
     };
 
@@ -120,7 +205,11 @@ export async function startMastodonOauth(
       mode,
     }),
     pkceVerifier,
+    handle,
+    username,
     instance,
+    host,
+    clientKey,
   });
   await db
     .insertInto('at_oauth_state')
@@ -158,7 +247,7 @@ export const mastodon: FastifyPluginAsync = async (rawApp) => {
     },
     async function (request, reply) {
       const { db, playerService } = rawApp.blueskyBridge;
-      const state = await db
+      const stateRecord = await db
         .deleteFrom('at_oauth_state')
         .returningAll()
         .where('key', '=', request.query.state)
@@ -168,11 +257,17 @@ export const mastodon: FastifyPluginAsync = async (rawApp) => {
         reply,
         app.blueskyBridge,
         'mastodon',
-        state,
+        stateRecord,
         async () => {
-          const { pkceVerifier, instance } = JSON.parse(state.data);
+          const {
+            pkceVerifier,
+            username: initialUsername,
+            instance: initialInstance,
+            host,
+            clientKey,
+          } = JSON.parse(stateRecord.data);
 
-          const tokenUrl = new URL('/oauth/token', `https://${instance}`).href;
+          const tokenUrl = new URL('/oauth/token', `https://${host}`).href;
 
           const baseUrl = process.env.PUBLIC_BASE_URL as string;
           const redirectUri = `${baseUrl}/mastodon/callback`;
@@ -180,7 +275,7 @@ export const mastodon: FastifyPluginAsync = async (rawApp) => {
           const client = await db
             .selectFrom('mastodon_client')
             .selectAll()
-            .where('instance', '=', instance)
+            .where('instance', '=', clientKey)
             .executeTakeFirstOrThrow();
 
           const { access_token, created_at } = tokenResponseSchema.parse(
@@ -200,17 +295,25 @@ export const mastodon: FastifyPluginAsync = async (rawApp) => {
 
           const verifyUrl = new URL(
             '/api/v1/accounts/verify_credentials',
-            `https://${instance}`
+            `https://${host}`
           ).href;
-          const { id: id_from_user, username } = mastoUserSchema.parse(
-            await w
-              .headers({
-                Authorization: `Bearer ${access_token}`,
-              })
-              .get(verifyUrl)
-              .json()
-          );
-          const mastodon_account = `${username}@${instance}`;
+          const { id: id_from_user, username: resolvedUsername } =
+            mastoUserSchema.parse(
+              await w
+                .headers({
+                  Authorization: `Bearer ${access_token}`,
+                })
+                .get(verifyUrl)
+                .json()
+            );
+          if (
+            resolvedUsername.toLowerCase() !== initialUsername.toLowerCase()
+          ) {
+            throw new Error(
+              `Auth stated for ${initialUsername}@${initialInstance} but when resolved was ${resolvedUsername}`
+            );
+          }
+          const mastodon_account = `${resolvedUsername}@${initialInstance}`;
           const santaMastodonHandle = playerService.santaMastodonHandle;
           if (
             mastodon_account.toLowerCase() === santaMastodonHandle.toLowerCase()
@@ -234,18 +337,24 @@ export const mastodon: FastifyPluginAsync = async (rawApp) => {
               .execute();
           }
 
+          const bskyHandle = getBridgedHandle(mastodon_account);
+          let did = '';
+          try {
+            const response = await unauthenticatedAgent.resolveHandle({
+              handle: bskyHandle,
+            });
+            did = response.data.did;
+          } catch (error) {
+            request.log.error(error);
+            throw new Error(
+              `Unable to resolve Bluesky account ${bskyHandle} for account ${mastodon_account}. Have you set up bridgy fed?`
+            );
+          }
+
           const following =
             await playerService.lookupMastodonFollowing(mastodon_account);
-
-          // FIXME handle errors for can't resolve and similar
-          const bskyHandle = `${mastodon_account.replace(/[_~]/g, '-').replace('@', '.')}.ap.brid.gy`;
-          const response = await unauthenticatedAgent.resolveHandle({
-            handle: bskyHandle,
-          });
-
-
           return {
-            did: response.data.did,
+            did,
             attributes: {
               mastodon_account,
               ...following,
--- src/routes/player/new-player.ts
+++ src/routes/player/new-player.ts
@@ -1,7 +1,10 @@
 import type { FastifyPluginAsync } from 'fastify';
-import { ForbiddenError } from 'http-errors-enhanced';
+import { BadRequestError, ForbiddenError } from 'http-errors-enhanced';
 import type { ZodTypeProvider } from 'fastify-type-provider-zod';
 import { z } from 'zod';
+import { getBridgedHandle, resolveMastodonHandle } from '../mastodon/index.js';
+import type { InsertObject } from 'kysely';
+import type { DatabaseSchema } from '../../lib/database/schema.js';
 
 export const newPlayer: FastifyPluginAsync = async (rawApp) => {
   const app = rawApp.withTypeProvider<ZodTypeProvider>();
@@ -10,6 +13,7 @@ export const newPlayer: FastifyPluginAsync = async (rawApp) => {
     {
       schema: {
         body: z.object({
+          player_type: z.enum(['bluesky', 'mastodon']),
           handle: z.string(),
           address: z.string(),
           delivery_instructions: z.string(),
@@ -21,21 +25,40 @@ export const newPlayer: FastifyPluginAsync = async (rawApp) => {
         throw new ForbiddenError();
       }
       const { playerService, db, santaAgent } = app.blueskyBridge;
+      let handle = request.body.handle.replace(/^@/, '').trim();
+      let additionalAttributes: Partial<
+        InsertObject<DatabaseSchema, 'player'>
+      > = {};
+      if (request.body.player_type === 'mastodon') {
+        const [, host] = handle.split('@');
+        const { handle: mastodon_account } = await resolveMastodonHandle(
+          handle,
+          host as string
+        );
+        handle = getBridgedHandle(mastodon_account);
+        const following =
+          await playerService.lookupMastodonFollowing(mastodon_account);
+        additionalAttributes = {
+          mastodon_account,
+          ...following,
+        };
+      } else if (handle.endsWith('.ap.brid.gy')) {
+        throw new BadRequestError('Mastodon handle');
+      } else {
+        handle = handle.replace(/@/g, '.');
+      }
       const agent = await santaAgent();
       const resolveHandleResult = await agent.resolveHandle({
-        handle: request.body.handle,
+        handle,
       });
       const playerDid = resolveHandleResult.data.did;
-      await playerService.createPlayer(
-        playerDid,
-        request.body.handle.endsWith('.ap.brid.gy') ? 'mastodon' : 'bluesky',
-        {
-          address: request.body.address || null,
-          delivery_instructions: request.body.delivery_instructions || null,
-          game_mode: 'Regular',
-          max_giftees: 1,
-        }
-      );
+      await playerService.createPlayer(playerDid, request.body.player_type, {
+        ...additionalAttributes,
+        address: request.body.address || null,
+        delivery_instructions: request.body.delivery_instructions || null,
+        game_mode: 'Regular',
+        max_giftees: 1,
+      });
 
       const updatedPlayer = await db
         .selectFrom('player')
