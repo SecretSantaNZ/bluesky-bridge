30b0570a612a76327a265b0b490104db221281bf
Ed Costello <edc.open@gmail.com>
2025-05-24 20:38:49 +1200

    Button to poke inactive santas

--- src/routes/view/admin/without-gifts.ts
+++ src/routes/view/admin/without-gifts.ts
@@ -3,6 +3,27 @@ import type { ZodTypeProvider } from 'fastify-type-provider-zod';
 import { sql } from 'kysely';
 import { z } from 'zod';
 import { queryFullMatch } from '../../../lib/database/index.js';
+import type { ExpressionBuilder } from 'kysely';
+import type { DatabaseSchema } from '../../../lib/database/schema.js';
+import type { OperandExpression } from 'kysely';
+import type { SqlBool } from 'kysely';
+
+function filterToInactiveMatches(
+  eb: ExpressionBuilder<DatabaseSchema, 'match'>
+): OperandExpression<SqlBool> {
+  return eb.and([
+    eb('match.deactivated', 'is', null),
+    eb('match.match_status', '=', 'locked'),
+    eb.or([
+      eb('match.tracking_count', '=', 0),
+      eb('match.tracking_missing_count', '>', 0),
+    ]),
+    eb('match.nudge_present_update_count', '=', 0),
+    eb('match.contacted', 'is', null),
+    eb('match.followup_action', 'is', null),
+    eb('match.super_santa_match', '=', 0),
+  ]);
+}
 
 export const withoutGifts: FastifyPluginAsync = async (rawApp) => {
   const app = rawApp.withTypeProvider<ZodTypeProvider>();
@@ -19,31 +40,37 @@ export const withoutGifts: FastifyPluginAsync = async (rawApp) => {
     },
     async function (request, reply) {
       const { db } = this.blueskyBridge;
-      const [playersWhoCanHaveMoreGifees, matches] = await Promise.all([
-        db
-          .selectFrom('player')
-          .select([
-            'handle',
-            'did',
-            'avatar_url',
-            'note_count',
-            'giftee_count',
-            'giftee_for_count',
-            'max_giftees',
-          ])
-          .where('giftee_count_status', '=', 'can_have_more')
-          .where('signup_complete', '=', 1)
-          .orderBy(
-            sql`giftee_count - (case when giftee_for_count > 0 then 1 else 0 end) asc`
-          )
-          .orderBy('giftee_count')
-          .orderBy(sql`random()`)
-          .execute(),
-        queryFullMatch(db)
-          .where('match.match_status', '=', 'locked')
-          .orderBy('match.id asc')
-          .execute(),
-      ]);
+      const [playersWhoCanHaveMoreGifees, matches, toMessageCount] =
+        await Promise.all([
+          db
+            .selectFrom('player')
+            .select([
+              'handle',
+              'did',
+              'avatar_url',
+              'note_count',
+              'giftee_count',
+              'giftee_for_count',
+              'max_giftees',
+            ])
+            .where('giftee_count_status', '=', 'can_have_more')
+            .where('signup_complete', '=', 1)
+            .orderBy(
+              sql`giftee_count - (case when giftee_for_count > 0 then 1 else 0 end) asc`
+            )
+            .orderBy('giftee_count')
+            .orderBy(sql`random()`)
+            .execute(),
+          queryFullMatch(db)
+            .where('match.match_status', '=', 'locked')
+            .orderBy('match.id asc')
+            .execute(),
+          db
+            .selectFrom('match')
+            .select(({ fn }) => fn.countAll().as('cnt'))
+            .where((eb) => filterToInactiveMatches(eb))
+            .executeTakeFirstOrThrow(),
+        ]);
       const pageData = {
         playersWhoCanHaveMoreGifees,
         matches,
@@ -55,11 +82,43 @@ export const withoutGifts: FastifyPluginAsync = async (rawApp) => {
         'admin/without-gifts.ejs',
         {
           pageData,
+          toMessageCount: toMessageCount.cnt,
         },
         {
           layout: 'layouts/base-layout.ejs',
         }
       );
+    }
+  );
+
+  app.post(
+    '/without-gifts/poke',
+    {
+      schema: {
+        body: z.object({}),
+      },
+    },
+    async function (request, reply) {
+      const { db } = this.blueskyBridge;
+
+      const result = await db
+        .updateTable('player')
+        .set({
+          next_player_dm: 'poke-inactive',
+        })
+        .where((eb) =>
+          eb(
+            'player.id',
+            'in',
+            eb
+              .selectFrom('match')
+              .select('match.santa')
+              .where((eb) => filterToInactiveMatches(eb))
+          )
+        )
+        .executeTakeFirst();
+
+      return reply.send(`DMs will be sent to ${result.numUpdatedRows} Santas`);
     }
   );
 };
