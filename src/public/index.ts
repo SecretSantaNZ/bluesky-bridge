4e5305688ec2596ce2506c6cefb941a290b92001
Ed Costello <edc.open@gmail.com>
2025-10-14 21:54:00 +1300

    Mostly package alpine, fetch remaining

--- src/public/index.ts
+++ src/public/index.ts
@@ -3,30 +3,45 @@ export * from './disableFields.js';
 export * from './utils.js';
 export * from '../lib/dates.js';
 
-(function () {
-  const mql = window.matchMedia('(max-width: 1024px)');
-  function screenTest(e: MediaQueryList | MediaQueryListEvent) {
-    // @ts-expect-error this field is not declared
-    window.ssIsMobileWidth = e.matches;
-    window.dispatchEvent(
-      new CustomEvent('ss-is-mobile-width-changed', { detail: e.matches })
-    );
-  }
-  mql.addEventListener('change', screenTest);
-  screenTest(mql);
+import Alpine from 'alpinejs';
+import morph from '@alpinejs/morph';
+import ajax from '@imacrayon/alpine-ajax';
+import persist from '@alpinejs/persist';
 
-  window.addEventListener('ajax:before', function (evt) {
-    startRequest(evt.target as Element);
-  });
-  window.addEventListener('ajax:after', function (evt) {
-    endRequest(evt.target as Element);
-  });
-  window.addEventListener('ajax:send', function (evt) {
-    const event = evt as CustomEvent;
-    const target = event.target as Element;
-    const errorTarget = target.getAttribute('x-target.error');
-    if (errorTarget) {
-      event.detail.headers['x-ssnz-error-target'] = errorTarget;
-    }
-  });
-})();
+declare global {
+  interface Window {
+    Alpine: typeof Alpine;
+  }
+}
+window.Alpine = Alpine;
+Alpine.plugin(morph);
+Alpine.plugin(ajax);
+Alpine.plugin(persist);
+
+Alpine.start();
+
+const mql = window.matchMedia('(max-width: 1024px)');
+function screenTest(e: MediaQueryList | MediaQueryListEvent) {
+  // @ts-expect-error this field is not declared
+  window.ssIsMobileWidth = e.matches;
+  window.dispatchEvent(
+    new CustomEvent('ss-is-mobile-width-changed', { detail: e.matches })
+  );
+}
+mql.addEventListener('change', screenTest);
+screenTest(mql);
+
+window.addEventListener('ajax:before', function (evt) {
+  startRequest(evt.target as Element);
+});
+window.addEventListener('ajax:after', function (evt) {
+  endRequest(evt.target as Element);
+});
+window.addEventListener('ajax:send', function (evt) {
+  const event = evt as CustomEvent;
+  const target = event.target as Element;
+  const errorTarget = target.getAttribute('x-target.error');
+  if (errorTarget) {
+    event.detail.headers['x-ssnz-error-target'] = errorTarget;
+  }
+});
